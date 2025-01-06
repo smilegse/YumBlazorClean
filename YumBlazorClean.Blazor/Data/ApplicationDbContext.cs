430e990376f930a2da74c090f6aa9689d70faf7d
Md. Abu Bakar Siddique <abms.siddique@gmail.com>
2025-01-06 09:48:12 +0600

    Add project files.

---
+++ YumBlazorClean.Blazor/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace YumBlazorClean.Blazor.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    {
+    }
+}
