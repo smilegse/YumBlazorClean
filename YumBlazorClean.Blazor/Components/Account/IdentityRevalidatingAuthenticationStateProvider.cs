5ce32347d35b3bf7355ceefd59bd2f1bed59d13a
smilegse <abms.siddique@gmail.com>
2025-01-07 06:56:56 +0600

    Comitted 83: Dependency with Project

--- YumBlazorClean.Blazor/Components/Account/IdentityRevalidatingAuthenticationStateProvider.cs
+++ YumBlazorClean.Blazor/Components/Account/IdentityRevalidatingAuthenticationStateProvider.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Server;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Options;
-using YumBlazorClean.Blazor.Data;
 using YumBlazorClean.Domain.Entities;
 
 namespace YumBlazorClean.Blazor.Components.Account
